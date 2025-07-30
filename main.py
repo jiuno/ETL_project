4d1617a8e036c7dc0188b6e128045ea2757f59a8
jiuno <bruno.alliani@gmail.com>
2025-07-29 21:50:15 -0300

    renamed sources

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 ## main.py
-import sqlite3
-from etl.sources.source_1.extract import read_excel
-from etl.sources.source_1.transform import * #funciones
-from etl.sources.source_1.load import save_to_sqlite
+from etl.sources.excels_1.extract import read_excel
+from etl.sources.excels_1.transform import * #funciones
+from etl.sources.excels_1.load import save_to_sqlite
 from etl.db import get_connection, init_schema
 
 
